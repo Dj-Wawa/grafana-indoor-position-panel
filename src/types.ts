93997447189632f01ed82c0536f8f5e78b6433f5
Hannes Iven <hannes.iven@student.uni-luebeck.de>
2025-12-09 00:03:00 +0100

    cleanup: types.ts - remove unused

--- src/types.ts
+++ src/types.ts
@@ -1,33 +1,24 @@
 export interface SimpleOptions {
   imageUrl: string
-  coordinates:{
-    topLeft:{
+  coordinates: {
+    topLeft: {
       lat: number
       long: number
     }
-    bottomLeft:{
+    bottomLeft: {
       lat: number
       long: number
     }
-    topRight:{
+    topRight: {
       lat: number
       long: number
     }
   }
-  /*topLeftLat: number
-  topLeftLong: number
-  topRightLat: number
-  topRightLong: number
-  bottomRightLat: number
-  bottomRightLong: number*/
   minElevation: number
   maxElevation: number
-  destination:{
+  destination: {
     lat: number
     long: number
     elevation: number
   }
-  /*destLat: number
-  destLong: number
-  destElevation: number*/
 }
